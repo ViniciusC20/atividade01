d43b878efa8765142663334c9239ff22ae820d7e
Vinicius C <viniciuscorreia54321@gmail.com>
2021-06-21 19:08:12 -0300

    quase

--- CRUD/create.php
+++ CRUD/create.php
@@ -4,12 +4,18 @@ $username = "root";
 $password = "";
 $dbname = "1atividade";
 try {
-    
+
   $conn = new PDO("mysql:host=$servername;dbname=$dbname; charset=utf8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Conectado com Sucesso!!!";
+  $sql = "INSERT INTO atividade01 (Nome, Disciplina, nota1, nota2)
+  VALUES ('Vinicius', 'Redes', '10', '9')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Cadastro feito com sucesso, você já pode olhar algumas notas!";
 } catch(PDOException $e) {
-  echo "Falha na Conexão: " . $e->getMessage();
+  echo $sql . "<br>" . $e->getMessage();
 }
+
+$conn = null;
 ?>
