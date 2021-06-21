00ad20fa135e863c9a2d736b312b10bcb5263f6c
Vinicius C <viniciuscorreia54321@gmail.com>
2021-06-21 15:56:46 -0300

    .

---
+++ db/conect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php_vinicius";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname; charset=utf-8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Conectado com Sucesso!!!";
+} catch(PDOException $e) {
+  echo "Falha na Conexão: " . $e->getMessage();
+}
+?>
