22ede1002eb73a6ae1b0f22c61c0a4d750ca0407
Vinicius C <viniciuscorreia54321@gmail.com>
2021-06-22 15:50:16 -0300

    ATUALIZAÇÃO

--- index.php
+++ index.php
@@ -16,9 +16,17 @@
       <h2>R<a href="CRUD/read.php">ead</a></h2>
       <h2>U<a href="CRUD/update.php">pdate</a></h2>
       <h2>D<a href="CRUD/delete.php">elete</a></h2>
+      </head>
+
+    <form name ="Form_cad" action="CRUD/create.php">
+        <input class="form-control form-control-lg" type="text" placeholder="Id" aria-label=".form-control-lg example">
+<input class="form-control" type="text" placeholder="Nome" aria-label="default input example"><br>
+<input class="form-control form-control-sm" type="text" placeholder="Disciplina" aria-label=".form-control-sm example">
+<input class="form-control form-control-lg" type="text" placeholder="Nota 1" aria-label=".form-control-lg example"><br>
+<input class="form-control" type="text" placeholder="Nota 2" aria-label="default input example"><br><br>
       
-      
-      
-      
+        <button type="submit" class="btn btn-primary">Alterar/Cadastrar</button>
+      </form>
+      <hr>
 </body >
 </html >
